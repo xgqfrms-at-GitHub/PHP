97835b2ec0c47cd3f0aa5e46c4fcf656a09e8578
xgqfrms <xgqfrms@outlook.com>
2016-04-10 22:52:09 +0800

    php7-removed

---
+++ php7/php7-removed-extensions.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * http://www.runoob.com/php/php-removed-extensions.html
+ * 
+ * PHP 7 移除的扩展
+ * 
+ * PHP 7+ 版本移除了以下扩展
+ * 1.  ereg
+ * 2.  mssql
+ * 3.  mysql
+ * 4.  sybase_ct 
+ * 
+ */
+
+ ?>
---
+++ php7/php7.php
@@ -0,0 +1,38 @@
+<?php 
+/**
+ * http://www.runoob.com/php/php7-new-features.html
+ * 
+ * PHP 7 新特性
+ * PHP 7+ 版本极大地改进了性能，在一些WordPress基准测试当中，性能可以达到PHP 5.6的3倍。
+ * 
+ * PHP 7+ 版本新加特性如下表所示：
+ * 序号 	内容
+ * 1 		PHP 标量类型与返回值类型声明
+ * 2 		PHP NULL 合并运算符
+ * 3 		PHP 太空船运算符（组合比较符）
+ * 4 		PHP 常量数组
+ * 5 		PHP 匿名类
+ * 6 		PHP Closure::call()
+ * 7 		PHP 过滤 unserialize()
+ * 8 		PHP IntlChar()
+ * 9 		PHP CSPRNG
+ * 10 		PHP 7 异常
+ * 11 		PHP 7 use 语句
+ * 12 		PHP 7 错误处理
+ * 13 		PHP intdiv() 函数
+ * 14 		PHP 7 Session 选项
+ * 15 		PHP 7 废弃特性
+ * 16 		PHP 7 移除的扩展
+ * 17 		PHP 7 移除的 SAPI
+ * 
+ */
+
+# comment template
+
+/**
+ * 
+ * 
+ */
+
+
+ ?>
