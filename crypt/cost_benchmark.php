4eb292a3d0e95c97c5c8ac599b68beec3262ec3d
xgqfrms <xgqfrms@outlook.com>
2016-04-12 20:50:53 +0800

    ok

--- crypt/cost_benchmark.php
+++ crypt/cost_benchmark.php
@@ -155,16 +155,30 @@ echo "\n<br/>";
 echo "双引号，错误\n<br/>";
 echo  strlen($str_2);
 
+
+//error
+// id INT(32) NOT NULL UNSIGNED KEY AUTO_INCREMENT,
+
+//  register_time  VARCHAR(255) NOT NULL COMMENT '注册时间',)ENGINE=INNODB  DEFAULT CHARSET=utf8;
+
+//right
+//id INT(32) UNSIGNED KEY AUTO_INCREMENT,
+//id INT(32) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+
+//  register_time  VARCHAR(255) NOT NULL COMMENT '注册时间')ENGINE=INNODB  DEFAULT CHARSET=utf8;
+//  register_time  VARCHAR(255) NOT NULL COMMENT '注册时间')ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+$sql = <<<EOF
 CREATE TABLE IF NOT EXISTS users( 
-  id INT(32) NOT NULL UNSIGNED KEY AUTO_INCREMENT,
+  id INT(32) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(255) NOT NULL UNIQUE COMMENT '用户名', 
   password VARCHAR(255) NOT NULL COMMENT'密码',
   email VARCHAR(255) NOT NULL UNIQUE COMMENT '邮箱',
   token VARCHAR(255) NOT NULL UNIQUE COMMENT '帐号激活码',
   token_expire INT(32) NOT NULL COMMENT '激活码有效期',
   status TINYINT(1) NOT NULL DEFAULT '0' COMMENT '状态,0-未激活,1-已激活',
-  register_time  VARCHAR(255) NOT NULL COMMENT '注册时间',
-) ENGINE=INNODB  DEFAULT CHARSET=utf8; 
+  register_time  VARCHAR(255) NOT NULL COMMENT '注册时间'
+)ENGINE=INNODB  DEFAULT CHARSET=utf8; 
+EOF;
 
 
 
@@ -178,4 +192,5 @@ CREATE TABLE IF NOT EXISTS pdo_users(
 	       status tinyint(1) NOT NULL,
 	       register_time VARCHAR(255) NOT NULL
 	       );
+
 ?>
