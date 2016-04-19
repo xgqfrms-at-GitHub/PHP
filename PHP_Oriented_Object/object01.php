304585b4420599e7bc76afc4a6c06083d895c963
xgqfrms <xgqfrms@outlook.com>
2016-04-19 14:51:17 +0800

    ok

--- PHP_Oriented_Object/object01.php
+++ PHP_Oriented_Object/object01.php
@@ -17,10 +17,17 @@ class NBAPlayer{
     protected $secret;
 
     //construct method
-    function __construct(){
+    function __construct($name,$height,$weight,$team,$player_num){
     	$var = "100 Million";
     	print_r(var, return)
+    	# this 伪变量，对象自身
+    	$this->name = $name;
+    	$this->height = $height;
+    	$this->weight = $weight;
+    	$this->team = $team;
+    	$this->player_num = $player_num;
     }
+    # construct will be auto call when object instance 
 
     //public methods
     public function run(){
@@ -37,6 +44,14 @@ class NBAPlayer{
     }
 }
 
-
+$jordan = new NBAPlayer("Jordan","1.98m","98kg","Bull","23");
+#实例化，构造方法(参数list)！
+$jordan->run();
+$jordan->jump();
+$jordan->dribble();
+$jordan->slam_dunk();
+#
+$var_name = $jordan->name;
+echo "name:{$var_name}\n";
 
  ?>
