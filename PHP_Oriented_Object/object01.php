c214a1d1be35107ef0ae63b60ce59868a456bb05
xgqfrms <xgqfrms@outlook.com>
2016-04-19 14:39:01 +0800

    __construct
    construct method

--- PHP_Oriented_Object/object01.php
+++ PHP_Oriented_Object/object01.php
@@ -1,11 +1,40 @@
 <?php 
 //class
 class NBAPlayer{
+	//public attributes
 	public $name;
 	public $height;
 	public $weight;
 	public $team;
 	public $player_num;
+    //private attributes
+    private $blood_group;
+    private $hobby;
+    private $families;
+    //protected attributes
+    protected $income;
+    protected $health;
+    protected $secret;
+
+    //construct method
+    function __construct(){
+    	$var = "100 Million";
+    	print_r(var, return)
+    }
+
+    //public methods
+    public function run(){
+    	echo "Running\n";
+    }
+    public function jump(){
+    	echo "Jumping\n";
+    }
+    public function dribble(){
+    	echo "Dribbling\n";
+    }
+    public function slam_dunk(){
+    	echo "Slam dunk\n";
+    }
 }
 
 
