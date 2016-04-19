fe7b4fefa1db71cd83d0ef0086be8aca3336c74b
xgqfrms <xgqfrms@outlook.com>
2016-04-19 15:02:37 +0800

    destruct
    destruct_methods

--- PHP_Oriented_Object/object01.php
+++ PHP_Oriented_Object/object01.php
@@ -29,6 +29,20 @@ class NBAPlayer{
     }
     # construct will be auto call when object instance 
 
+    //destruct method
+    // function __destruct(){
+    function __destruct($name,$height,$weight,$team,$player_num){
+    	$var = "100 Million";
+    	print_r(var, return)
+    	# this 伪变量，对象自身
+    	$this->name = $name;
+    	$this->height = $height;
+    	$this->weight = $weight;
+    	$this->team = $team;
+    	$this->player_num = $player_num;
+    }
+    # construct will be auto call when object instance 
+
     //public methods
     public function run(){
     	echo "Running\n";
@@ -42,6 +56,12 @@ class NBAPlayer{
     public function slam_dunk(){
     	echo "Slam dunk\n";
     }
+
+    //private methods
+    private function pwd($new_pwd){
+    	$pwd = $new_pwd;
+    	echo "pwd:{$pwd}";
+    }
 }
 
 $jordan = new NBAPlayer("Jordan","1.98m","98kg","Bull","23");
@@ -53,5 +73,9 @@ $jordan->slam_dunk();
 #
 $var_name = $jordan->name;
 echo "name:{$var_name}\n";
+
+// 显式： $jordan = null ; # 直接调用 析构函数；
+// 隐式： //$jordan = null ; # 在函数执行结束时，自动调用 析构函数；
+
 
  ?>
